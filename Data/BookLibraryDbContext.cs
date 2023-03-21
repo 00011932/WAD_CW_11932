7893eadd4e344d437fc7c9663b3f60424ac47ae4
Temurbek <646botirovtemur@gmail.com>
2023-03-21 12:46:42 +0500

    initial commit

---
+++ Data/BookLibraryDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookLibrary.Data
+{
+    public class BookLibraryDbContext
+    {
+      
+            //public BookLibraryDbContext(DbContextOptions<BookLibraryDbContext> options) : base(options)
+            //{
+            //    Database.EnsureCreated();
+            //}
+            
+        
+    }
+}
